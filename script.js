1294ccfe7728ece71d2bbebdff0e93354c86c582
Saroj Shrestha <sarojshrestha@Sarojs-iMac.local>
2024-01-19 08:50:38 -0700

    udtated

--- script.js
+++ script.js
@@ -1,26 +1,109 @@
 $('document').ready(function () {
   var inputBox = $('#input-box');
   var listContainer = $('#list-container');
+  
+  // render the data from api
+$.ajax({
+    type: 'GET',
+    url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1',
+    dataType: 'json',
+    success: function (response, textStatus) {
+      console.log(response)
+      response.tasks.forEach(function(task){
+        // if task.completed === 'true' linethrough
+        var li = $('<li>').text(task.content).attr("id", task.id);
+        var checkbox = $('<input type="checkbox">');
+        var span = $('<span>').text('x');
+        if(task.completed === true){
+          checkbox.attr("checked")
+        }
+        listContainer.append(li);
+        li.append(checkbox);
+        li.append(span);
+        
+      })
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });
 
+  // add a new task
   $('button').on('click', function () {
   var inputValue = inputBox.val(); 
   if (inputValue === "") {
     alert("You must add a task.");
   } else {
-    var li = $('<li>').text(inputValue); 
-    listContainer.append(li);
-    
-    var span = $('<span>').text('x');
-    li.append(span);
+    $.ajax({
+      type: 'POST',
+      url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify({
+        task: {
+          content: inputValue
+        }
+      }),
+      success: function (response, textStatus) {
+        console.log(response);
+        var li = $('<li>').text(inputValue).attr("id", response.task.id); 
+        listContainer.append(li);
+        
+        var span = $('<span>').text('x');
+        li.append(span);
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
     }
     inputBox.val(""); 
   });
   
   listContainer.on('click', function (e) {
+    var id = e.target.parentElement.id
+    console.log(id)
+    $.ajax({
+      type: 'PUT',
+      url: `https://fewd-todolist-api.onrender.com/tasks/${id}?api_key=1`,
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify({
+        task: {
+          completed: true
+        }
+      }),
+      success: function (response, textStatus) {
+        console.log('yay!' + response);
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+
     if (e.target.tagName === 'LI') {
+      console.log(e.target.classList)
+
       e.target.classList.toggle('checked');
-    } else if (e.target.tagName === 'SPAN')  {
+      // make put call to update
+    } else if (e.target.tagName === 'SPAN')  { 
+      // make detele call to remove
+      $.ajax({
+        type: 'DELETE',
+        url: `https://fewd-todolist-api.onrender.com/tasks/${id}?api_key=1`,
+        success: function (response, textStatus) {
+          console.log(response);
+        },
+        error: function (request, textStatus, errorMessage) {
+          console.log(errorMessage);
+        }
+      });
+
       e.target.parentElement.remove();
+
+
     }
   })
 }); 
+
+
