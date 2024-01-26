6c65a0ccf66a4365009b7351a87bf1d4688f9bc4
Saroj Shrestha <saroj717@yahoo.com>
2024-01-26 15:10:22 -0700

    some changes

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $('document').ready(function () {
   // render the data from api
 $.ajax({
     type: 'GET',
-    url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1',
+    url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1154',
     dataType: 'json',
     success: function (response, textStatus) {
       console.log(response)
@@ -15,7 +15,8 @@ $.ajax({
         var checkbox = $('<input type="checkbox">');
         var span = $('<span>').text('x');
         if(task.completed === true){
-          checkbox.attr("checked");
+          console.log('true');
+          checkbox.prop("checked", true);
         }
 
         listContainer.append(li);
@@ -37,7 +38,7 @@ $.ajax({
   } else {
     $.ajax({
       type: 'POST',
-      url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1',
+      url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1154',
       contentType: 'application/json',
       dataType: 'json',
       data: JSON.stringify({
@@ -64,20 +65,23 @@ $.ajax({
   });
   
   listContainer.on('click', function (e) {
+    var check = e.target.prop("checked");
+    console.log(check);
     var id = e.target.parentElement.id
     console.log(id)
+
     $.ajax({
       type: 'PUT',
-      url: `https://fewd-todolist-api.onrender.com/tasks/${id}?api_key=1`,
+      url: `https://fewd-todolist-api.onrender.com/tasks/${id}/mark_complete?api_key=1154`,
       contentType: 'application/json',
       dataType: 'json',
       data: JSON.stringify({
         task: {
-          completed: true
+          completed: completed
         }
       }),
       success: function (response, textStatus) {
-        console.log('yay!' + response);
+        console.log('Task updated:', response);
       },
       error: function (request, textStatus, errorMessage) {
         console.log(errorMessage);
@@ -89,11 +93,12 @@ $.ajax({
 
       e.target.classList.toggle('checked');
       // make put call to update
-    } else if (e.target.tagName === 'SPAN')  { 
+    } else 
+    if (e.target.tagName === 'SPAN')  { 
       // make detele call to remove
       $.ajax({
         type: 'DELETE',
-        url: `https://fewd-todolist-api.onrender.com/tasks/${id}?api_key=1`,
+        url: `https://fewd-todolist-api.onrender.com/tasks/${id}?api_key=1154`,
         success: function (response, textStatus) {
           console.log(response);
         },
