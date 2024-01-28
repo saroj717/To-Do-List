2e59c37daf81ea761f2c172eb3ee269156ecfa2f
Saroj Shrestha <saroj717@yahoo.com>
2024-01-27 22:32:52 -0700

    some changes

--- script.js
+++ script.js
@@ -10,12 +10,10 @@ $.ajax({
     success: function (response, textStatus) {
       console.log(response)
       response.tasks.forEach(function(task){
-        // if task.completed === 'true' linethrough
         var li = $('<li>').text(task.content).attr("id", task.id);
         var checkbox = $('<input type="checkbox">');
         var span = $('<span>').text('x');
         if(task.completed === true){
-          console.log('true');
           checkbox.prop("checked", true);
         }
 
@@ -65,35 +63,48 @@ $.ajax({
   });
   
   listContainer.on('click', function (e) {
-    var check = e.target.prop("checked");
-    console.log(check);
+    var check = $(e.target).prop("checked");
+    console.log("check: " + check)
     var id = e.target.parentElement.id
     console.log(id)
-
-    $.ajax({
-      type: 'PUT',
-      url: `https://fewd-todolist-api.onrender.com/tasks/${id}/mark_complete?api_key=1154`,
-      contentType: 'application/json',
-      dataType: 'json',
-      data: JSON.stringify({
-        task: {
-          completed: completed
-        }
-      }),
-      success: function (response, textStatus) {
-        console.log('Task updated:', response);
-      },
-      error: function (request, textStatus, errorMessage) {
-        console.log(errorMessage);
-      }
-    });
-
-    if (e.target.tagName === 'LI') {
-      // console.log(e.target.classList)
-
+    if (check === true){
       e.target.classList.toggle('checked');
-      // make put call to update
-    } else 
+      $.ajax({
+        type: 'PUT',
+        url: `https://fewd-todolist-api.onrender.com/tasks/${id}/mark_complete?api_key=1154`,
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify({
+          task: {
+            completed: check
+          }
+        }),
+        success: function (response, textStatus) {
+          console.log(response);
+        },
+        error: function (request, textStatus, errorMessage) {
+          console.log(errorMessage);
+        }
+      });
+    } else if(check === false){
+      $.ajax({
+        type: 'PUT',
+        url: `https://fewd-todolist-api.onrender.com/tasks/${id}/mark_active?api_key=1154`,
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify({
+          task: {
+            completed: check
+          }
+        }),
+        success: function (response, textStatus) {
+          console.log(response);
+        },
+        error: function (request, textStatus, errorMessage) {
+          console.log(errorMessage);
+        }
+      });
+    } 
     if (e.target.tagName === 'SPAN')  { 
       // make detele call to remove
       $.ajax({
